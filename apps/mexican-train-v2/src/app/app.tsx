5871eece1013dd80167d560708a2e0f52eb883aa
Mark Dewey <mtdewey55@gmail.com>
2022-08-13 10:59:10 -0400

    initial commit

---
+++ apps/mexican-train-v2/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="mexican-train-v2" />
+      <div />
+    </>
+  );
+}
+
+export default App;
